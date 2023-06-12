7abf8f6317f894ebb68f347352a6a931d351b72a
Shaishav Paudel <morganhouse15@gmail.com>
2023-06-12 12:04:42 +0545

    RxjsLeelaYoutubeVideo44-HigherOrderMappingCompleted

--- src/app/components/operators/ajax/ajax.component.ts
+++ src/app/components/operators/ajax/ajax.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, map, pipe } from 'rxjs';
+import { Subscription, map, mergeMap, of, pipe } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -12,34 +12,61 @@ export class AjaxComponent implements OnInit, OnDestroy {
   subscription1!: Subscription;
   subscription2!: Subscription;
   ngOnInit(): void {
-    this.subscription = ajax('https://jsonplaceholder.typicode.com/posts/')
+    // this.subscription = ajax('https://jsonplaceholder.typicode.com/posts/')
+    //   .pipe(
+    //     map((response: any) => {
+    //       // let data = [];
+    //       // for (let post of response.response) {
+    //       //   data.push(post.title);
+    //       // }
+    //       // return data;
+    //       return response;
+    //     })
+    //   )
+    //   .subscribe((data) => console.log(data));
+    // this.subscription1 = ajax
+    //   .getJSON('https://jsonplaceholder.typicode.com/posts/')
+    //   .subscribe((data) => console.log(data));
+    // this.subscription2 = ajax({
+    //   url: 'https://jsonplaceholder.typicode.com/posts/',
+    //   method: 'POST',
+    //   headers: {
+    //     leela: 'Leela Web Dev',
+    //     'content-type': 'application/json',
+    //   },
+    //   body: {
+    //     title: 'Leela Web Dev',
+    //   },
+    // }).subscribe((data) => console.log(data));
+    //-----------------------------------------------------------------------------------------------
+    // this.subscription = of(1, 2, 3) //outerObservable
+    //   .pipe(
+    //     // map((value) => {
+    //     //   return of(value * 10); //  innerObservable
+    //     // }),
+    //     mergeMap((value) => {
+    //       return of(value * 10); //  innerObservable
+    //     })
+    //     // mergeMap((data) => data)
+    //   )
+    //   .subscribe((data) => {
+    //     // data.subscribe(
+    //     //   (data) => {
+    //     //   console.log(data);
+    //     // }
+    //     // );
+    //     console.log(data);
+    //   });
+    //-----------------------------------------------------------------------------------
+    this.subscription1 = of(1, 2, 3, 4)
       .pipe(
-        map((response: any) => {
-          // let data = [];
-          // for (let post of response.response) {
-          //   data.push(post.title);
-          // }
-          // return data;
-          return response;
+        mergeMap((id) => {
+          return ajax(`https://jsonplaceholder.typicode.com/posts/${id}`);
         })
       )
-      .subscribe((data) => console.log(data));
-
-    this.subscription1 = ajax
-      .getJSON('https://jsonplaceholder.typicode.com/posts/')
-      .subscribe((data) => console.log(data));
-
-    this.subscription2 = ajax({
-      url: 'https://jsonplaceholder.typicode.com/posts/',
-      method: 'POST',
-      headers: {
-        leela: 'Leela Web Dev',
-        'content-type': 'application/json',
-      },
-      body: {
-        title: 'Leela Web Dev',
-      },
-    }).subscribe((data) => console.log(data));
+      .subscribe((data) => {
+        console.log(data.response);
+      });
   }
 
   ngOnDestroy(): void {
