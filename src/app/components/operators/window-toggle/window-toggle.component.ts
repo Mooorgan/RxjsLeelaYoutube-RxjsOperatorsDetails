b1d8edf0cf2237217932373e6a2c1e586c87d4d1
Shaishav Paudel <morganhouse15@gmail.com>
2023-06-18 16:46:17 +0545

    RxjsLeelaYoutubeVideo110-windowToggleOperatorCompleted

--- src/app/components/operators/window-toggle/window-toggle.component.ts
+++ src/app/components/operators/window-toggle/window-toggle.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval, mergeMap, toArray, windowToggle } from 'rxjs';
 
 @Component({
   selector: 'app-window-toggle',
   templateUrl: './window-toggle.component.html',
-  styleUrls: ['./window-toggle.component.scss']
+  styleUrls: ['./window-toggle.component.scss'],
 })
-export class WindowToggleComponent {
+export class WindowToggleComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
 
+  ngOnInit(): void {
+    const source$ = interval(500);
+
+    this.subscription = source$
+      .pipe(
+        windowToggle(interval(1000), () => {
+          return interval(2000);
+        }),
+        mergeMap((val) => {
+          return val.pipe(toArray());
+        })
+      )
+
+      .subscribe({
+        next(value) {
+          console.log(value);
+        },
+        error(err) {
+          console.log(`Error isss ${err}`);
+        },
+        complete() {
+          console.log(' Completed');
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription && this.subscription.unsubscribe();
+  }
 }
