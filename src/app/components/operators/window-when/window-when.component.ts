f6a32bdc3f68bc84a2ad4f40fff94c66d8ffefdd
Shaishav Paudel <morganhouse15@gmail.com>
2023-06-18 17:04:06 +0545

    RxjsLeelaYoutubeVideo111-windowWhenOperatorCompleted

--- src/app/components/operators/window-when/window-when.component.ts
+++ src/app/components/operators/window-when/window-when.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Subscription,
+  interval,
+  mergeMap,
+  toArray,
+  windowToggle,
+  windowWhen,
+} from 'rxjs';
 
 @Component({
   selector: 'app-window-when',
   templateUrl: './window-when.component.html',
-  styleUrls: ['./window-when.component.scss']
+  styleUrls: ['./window-when.component.scss'],
 })
-export class WindowWhenComponent {
+export class WindowWhenComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
 
+  ngOnInit(): void {
+    const source$ = interval(500);
+
+    this.subscription = source$
+      .pipe(
+        windowWhen(() => interval(2000)),
+        mergeMap((val) => {
+          return val.pipe(toArray());
+        })
+      )
+
+      .subscribe({
+        next(value) {
+          console.log(value);
+        },
+        error(err) {
+          console.log(`Error isss ${err}`);
+        },
+        complete() {
+          console.log(' Completed');
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription && this.subscription.unsubscribe();
+  }
 }
