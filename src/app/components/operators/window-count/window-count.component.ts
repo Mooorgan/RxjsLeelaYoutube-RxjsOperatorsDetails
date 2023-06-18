703df6efec59c9ea6a477074d1caccc098768fab
Shaishav Paudel <morganhouse15@gmail.com>
2023-06-18 16:32:43 +0545

    RxjsLeelaYoutubeVideo108-windowCountOperatorCompleted

--- src/app/components/operators/window-count/window-count.component.ts
+++ src/app/components/operators/window-count/window-count.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval, mergeMap, toArray, windowCount } from 'rxjs';
 
 @Component({
   selector: 'app-window-count',
   templateUrl: './window-count.component.html',
-  styleUrls: ['./window-count.component.scss']
+  styleUrls: ['./window-count.component.scss'],
 })
-export class WindowCountComponent {
+export class WindowCountComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
 
+  ngOnInit(): void {
+    const source$ = interval(500);
+
+    this.subscription = source$
+      .pipe(
+        windowCount(3, 2),
+        mergeMap((val) => {
+          return val.pipe(toArray());
+        })
+      )
+
+      .subscribe({
+        next(value) {
+          console.log(value);
+        },
+        error(err) {
+          console.log(`Error isss ${err}`);
+        },
+        complete() {
+          console.log(' Completed');
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription && this.subscription.unsubscribe();
+  }
 }
--- src/app/components/operators/window-time/window-time.component.ts
+++ src/app/components/operators/window-time/window-time.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval, mergeMap, toArray, windowTime } from 'rxjs';
 
 @Component({
   selector: 'app-window-time',
   templateUrl: './window-time.component.html',
-  styleUrls: ['./window-time.component.scss']
+  styleUrls: ['./window-time.component.scss'],
 })
-export class WindowTimeComponent {
+export class WindowTimeComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
 
+  ngOnInit(): void {
+    const source$ = interval(500);
+
+    this.subscription = source$
+      .pipe(
+        windowTime(2000, 5000),
+        mergeMap((val) => {
+          return val.pipe(toArray());
+        })
+      )
+
+      .subscribe({
+        next(value) {
+          console.log(value);
+        },
+        error(err) {
+          console.log(`Error isss ${err}`);
+        },
+        complete() {
+          console.log(' Completed');
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription && this.subscription.unsubscribe();
+  }
 }
